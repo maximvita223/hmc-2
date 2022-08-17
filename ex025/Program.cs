74a2251769354d54d9f02955f946f9f8a9e21cc9
Maxim <maks.bidenko.96@mail.ru>
2022-08-18 00:36:22 +0300

    fixed

--- ex025/Program.cs
+++ ex025/Program.cs
@@ -1,9 +1,25 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-Console.Clear();
- 
-Console.WriteLine("Введите число которое хотите возвести в степень");
-double a = int.Parse(Console.ReadLine() ?? "0");
-Console.WriteLine("Введите степень в которую хотите возвести число");
-double b = int.Parse(Console.ReadLine() ?? "0");
+// Console.Clear();
 
-Console.Write(Math.Pow(a, b));
+// Console.WriteLine("Введите число   ");
+// double a = int.Parse(Console.ReadLine() ?? "0");
+// Console.WriteLine("Введите степень   ");
+// double b = int.Parse(Console.ReadLine() ?? "0");
+
+
+// Console.Write(Math.Pow(a, b));
+
+
+
+Console.Write("Введите число  ");
+int num = int.Parse(Console.ReadLine()?? "0");
+
+Console.Write("Введите степень  ");
+int n = int.Parse(Console.ReadLine()?? "0");
+
+int num_n = 1;
+for (int i = 0; i < n; i++)
+{
+    num_n *= num;
+}
+Console.WriteLine($"{num} ^ {n} = {num_n}");
