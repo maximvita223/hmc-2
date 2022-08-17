0dc6f79e26cb68d775af6b197b790060709c8be2
Maxim <maks.bidenko.96@mail.ru>
2022-08-17 17:16:27 +0300

    Сделал 29 задачу

---
+++ ex029/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array1 = new int[8];
+
+Random rnd = new Random();
+Console.Write ("Случайный массив из 8 элементов - ");
+for ( int i = 0; i < 8; i++)
+{
+    array1[i] = rnd.Next();
+    Console.Write (array1[i] + " ");
+}
