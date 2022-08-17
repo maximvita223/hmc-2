ab0d3779091bbdb6fcda5008a142c38850498306
Maxim <maks.bidenko.96@mail.ru>
2022-08-17 17:12:54 +0300

    Сделал 27 задачу

---
+++ ex027/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+if (num > 0)
+{
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+}
+else
+{
+    Console.WriteLine("Введите положительное число");
+    return;
+}
+Console.WriteLine(sum);
