552622ab4093187b509c8b4bfd2fcdc4d9ea0e74
Maxim <maks.bidenko.96@mail.ru>
2022-08-18 00:36:57 +0300

    fixed

--- ex029/Program.cs
+++ ex029/Program.cs
@@ -4,7 +4,7 @@ int[] array1 = new int[8];
 
 Random rnd = new Random();
 Console.Write ("Случайный массив из 8 элементов - ");
-for ( int i = 0; i < 8; i++)
+for ( int i = 0; i < array1.Length; i++)
 {
     array1[i] = rnd.Next();
     Console.Write (array1[i] + " ");
