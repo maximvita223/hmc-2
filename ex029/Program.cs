28a89006f89c2a98d5e23f8adeba980c77d0bf0a
Maxim <maks.bidenko.96@mail.ru>
2022-08-18 20:47:30 +0300

    fixed

--- ex029/Program.cs
+++ ex029/Program.cs
@@ -1,11 +1,14 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-int[] array1 = new int[8];
 
+// Console.WriteLine("Введите длину массива");
+// int size = Convert.ToInt32(Console.ReadLine());  Ввод длины массива пользователем
+// int[] array1 = new int[size];
+int[] array1 = new int[8];
 Random rnd = new Random();
-Console.Write ("Случайный массив из 8 элементов - ");
-for ( int i = 0; i < array1.Length; i++)
+Console.Write("Случайный массив из 8 элементов - ");
+for (int i = 0; i < array1.Length; i++)
 {
     array1[i] = rnd.Next();
-    Console.Write (array1[i] + " ");
+    Console.Write(array1[i] + " ");
 }
